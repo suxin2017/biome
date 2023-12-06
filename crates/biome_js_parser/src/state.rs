7d83a9bb47e32cae14fbbd8d09afb4c3e7b61283
suxin2017 <1107178482@qq.com>
2023-12-06 14:31:08 +0800

    fix: multiple function overloadbug

--- crates/biome_js_parser/src/state.rs
+++ crates/biome_js_parser/src/state.rs
@@ -44,11 +44,12 @@ impl ExportDefaultItemKind {
     const fn can_merge(a: &ExportDefaultItemKind, b: &ExportDefaultItemKind) -> bool {
         match (a, b) {
             // export default function a():void;
-            // export default function a(){
+            // export default function a(v: number):void;
+            // export default function a(v: any){
             // }
             (
                 ExportDefaultItemKind::FunctionOverload,
-                ExportDefaultItemKind::FunctionDeclaration,
+                ExportDefaultItemKind::FunctionDeclaration | ExportDefaultItemKind::FunctionOverload,
             ) => true,
             // export default function a(){};
             // export default interface A{};
